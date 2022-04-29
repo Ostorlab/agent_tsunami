4eb70fb9f45fc3c87a937bb52f80482fb46dcaca
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2022-04-29 10:31:12 +0000

    use agent_mock instead of manually mocking.

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,8 +8,7 @@ from ostorlab.runtimes import definitions as runtime_definitions
 from agent import tsunami_agent
 
 @pytest.fixture(scope='function', name='tsunami_agent')
-def fixture_tsunami_agent(mocker):
-    mocker.patch('ostorlab.agent.mixins.agent_healthcheck_mixin.AgentHealthcheckMixin.__init__', return_value=None)
+def fixture_tsunami_agent(agent_mock):
     with (pathlib.Path(__file__).parent.parent / 'ostorlab.yaml').open() as yaml_o:
 
         definition = agent_definitions.AgentDefinition.from_yaml(yaml_o)
