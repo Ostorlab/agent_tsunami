decd021a9f6d7a9f01efb7ff6fe4644c0a3a0e4d
mohsinenar <mnarmouche@gmail.com>
2023-03-22 18:27:53 +0000

    Test problem with comment coverage.

--- tests/vpn_test.py
+++ tests/vpn_test.py
@@ -1,13 +1,17 @@
 """Test VPN setup."""
+from unittest.mock import mock_open
+
 from pytest_mock import plugin
+
 from agent import tsunami_agent
 
 
-def _testVpnSetup_whenVpnCountryIsPresent_shouldCallSetupVpnSetupVpnWithRightConfig(
+def testVpnSetup_whenVpnCountryIsPresent_shouldCallSetupVpnSetupVpnWithRightConfig(
     fixture_tsunami_agent_with_vpn: tsunami_agent.AgentTsunami,
     mocker: plugin.MockerFixture,
 ) -> None:
     """Test when vpn country argument is provided should call setup vpn"""
+    mocker.patch("builtins.open", new_callable=mock_open())
     mocked_vpn_setup = mocker.patch("agent.vpn._exec_command", return_value=None)
 
     fixture_tsunami_agent_with_vpn.start()
